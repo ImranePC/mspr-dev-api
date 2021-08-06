fd32065673454c737ef6ed6934a3feac75484fd7
ImranePC <imranelevrai@gmail.com>
2021-08-06 13:44:38 +0200

    User auth route update

--- src/Application/Actions/User/UserAuthAction.php
+++ src/Application/Actions/User/UserAuthAction.php
@@ -11,7 +11,19 @@ class UserAuthAction extends UserAction
     public function action(): Response
     {
         $data = $this->parseBody();
-        $result = $this->user->select()->where('username', $data['username'])->first();
-        return $this->respondWithData(password_verify($data['password'], $result['password']));
+        $result["user"] = $this->user->select()->where('username', $data['username'])->first();
+        if (password_verify($data['password'], $result["user"]["password"])) {
+            $result["connection"] = true;
+            return $this->respondWithData($result);
+        } else {
+            unset($result["user"]);
+            if (!isset($result["user"])) {
+                $result["error"] = "FALSE_USERNAME";
+            } else {
+                $result["error"] = "FALSE_PASSWORD";
+            }
+            $result["connection"] = false;
+            return $this->respondWithData($result);
+        }
     }
 }
